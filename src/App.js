42f5448bfd365b0723715013db22b300250d80dd
amitmahlawat <amitmahlawat3@gmail.com>
2022-09-19 21:27:56 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import ExpenseItem from './Components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+ <h1>
+      this is visible
+    
+    </h1>
+    
+    <ExpenseItem></ExpenseItem>
+    <p>Food Rs 10</p>
+    <p>Petrol Rs 100</p>
+    <p>Movies Rs 200</p>
+    </div>
+   
+  )
+    
+  
+}
+
+export default App;
---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,10 @@
+function ExpenseItem(){
+    return <h2>Expense Item!</h2>
+
+    
+    
+}
+    
+
+
+export default ExpenseItem;
