720d9e5ead95317f57f3dce62827ccfb800d2128
amitmahlawat <amitmahlawat3@gmail.com>
2022-10-02 22:57:00 +0530

    cancel button added

--- src/Components/NewExpense/ExpenseForm.js
+++ src/Components/NewExpense/ExpenseForm.js
@@ -47,6 +47,7 @@ const ExpenseForm=(props  )=>{
             </div>
         </div>
         <div className="expense-item__actions">
+            <button onClick={props.onCancel}>Cancel</button>
             <button type='submit'>Add Expense</button>
         </div>
     </form>
